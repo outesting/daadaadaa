a73a69ca06d7af0e661ab9416aec51241d4d207c
Pekka <pekka.prokkinen@eficode.com>
2024-03-05 13:14:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-techdocs-cli generate --no-docker --verbose
-techdocs-cli publish --publisher-type azureBlobStorage --azureAccountName $AZURE_ACCOUNT_NAME --azureAccountKey $AZURE_ACCOUNT_KEY --storage-name $TECHDOCS_BLOB_STORAGE_NAME --entity default/Component/testing
+#techdocs-cli generate --no-docker --verbose
+#techdocs-cli publish --publisher-type azureBlobStorage --azureAccountName $AZURE_ACCOUNT_NAME --azureAccountKey $AZURE_ACCOUNT_KEY --storage-name $TECHDOCS_BLOB_STORAGE_NAME --entity default/Component/testing
+uname -a
